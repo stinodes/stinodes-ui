0d261787cf0fe87c1cafbeeabf49173d281bd8d8
stinodes <stijn@stinodes.dev>
2022-04-07 02:06:00 +0200

    feat(accordeon): added accordeon & resolved audit issues & upgraded storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,35 @@
-const path = require('path')
-const fs = require('fs')
-const { merge } = require('webpack-merge')
+const path = require('path');
+
+const fs = require('fs');
+
+const {
+  merge
+} = require('webpack-merge');
 
 function getPackageDir(filepath) {
-  let currDir = path.dirname(require.resolve(filepath))
+  let currDir = path.dirname(require.resolve(filepath));
+
   while (true) {
     if (fs.existsSync(path.join(currDir, 'package.json'))) {
-      return currDir
+      return currDir;
     }
-    const { dir, root } = path.parse(currDir)
+
+    const {
+      dir,
+      root
+    } = path.parse(currDir);
+
     if (dir === root) {
-      throw new Error(
-        `Could not find package.json in the parent directories starting from ${filepath}.`,
-      )
+      throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
     }
-    currDir = dir
+
+    currDir = dir;
   }
 }
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
-  ],
-
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
   webpackFinal: async config => {
     // Add emotion plugin & preset before react loader
     // And disable babelrc to prevent confusion
@@ -35,21 +39,23 @@ module.exports = {
       options: {
         babelrc: false,
         plugins: ['emotion'],
-        presets: [
-          ['react-app', { flow: false, typescript: true }],
-          require.resolve('@emotion/babel-preset-css-prop'),
-        ],
-      },
-    })
-
+        presets: [['react-app', {
+          flow: false,
+          typescript: true
+        }], require.resolve('@emotion/babel-preset-css-prop')]
+      }
+    });
     return merge(config, {
       resolve: {
         alias: {
           '@emotion/core': getPackageDir('@emotion/react'),
           '@emotion/styled': getPackageDir('@emotion/styled'),
-          'emotion-theming': getPackageDir('@emotion/react'),
-        },
-      },
-    })
+          'emotion-theming': getPackageDir('@emotion/react')
+        }
+      }
+    });
   },
-}
+  core: {
+    builder: "webpack5"
+  }
+};
